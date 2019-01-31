9b94c9afe06676a7da39a608a48ed4e31f5d125f
Maze X <maze@booking.com>
2019-01-31 10:06:03 +0100

    Docfix

--- afis/doc.go
+++ afis/doc.go
@@ -16,8 +16,11 @@ Split operates on data and returns information splitted data. Merge does
 just the opposite: uses the information stored in data to recover the original
 splitted data.
 
-References:
-  * AFsplitter reference implementation at http://clemens.endorphin.org/AFsplitter
-  * TKS1 paper at http://clemens.endorphin.org/TKS1-draft.pdf
+
+References
+
+AFsplitter reference implementation at http://clemens.endorphin.org/AFsplitter
+
+TKS1 paper at http://clemens.endorphin.org/TKS1-draft.pdf
 */
 package afis
